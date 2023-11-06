f09c88d60b3bcc472360d82c53ee750ab237f4bf
Bui Nam Ngoc <onggia23197@gmail.com>
2023-11-06 22:04:46 +0700

    push code Example 36

--- src/main/java/com/eazybytes/eazyschool/model/Contact.java
+++ src/main/java/com/eazybytes/eazyschool/model/Contact.java
@@ -1,12 +1,13 @@
 package com.eazybytes.eazyschool.model;
 
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
 
 /*
 @Data annotation is provided by Lombok library which generates getter, setter,
--- src/main/java/com/eazybytes/eazyschool/repository/HolidaysRepository.java
+++ src/main/java/com/eazybytes/eazyschool/repository/HolidaysRepository.java
@@ -10,7 +10,6 @@ type to the Spring context and indicate that given Bean is used to perform
 DB related operations and
 * */
 @Repository
-public interface HolidaysRepository extends CrudRepository<Holiday, String>  {
-
+public interface HolidaysRepository extends CrudRepository<Holiday, String> {
 
 }
