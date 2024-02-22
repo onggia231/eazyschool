d997b21f6a0b980eb604808d66685640e00dac97
Bui Nam Ngoc <onggia23197@gmail.com>
2024-02-22 23:48:46 +0700

    push code Example 47

--- src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
+++ src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
@@ -2,10 +2,14 @@ package com.eazybytes.eazyschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.eazybytes.eazyschool.proxy")
+@EnableJpaRepositories("com.eazybytes.eazyschool.repository")
+@EntityScan("com.eazybytes.eazyschool.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class EazyschoolApplication {
 
 	public static void main(String[] args) {
