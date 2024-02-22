41e174a384ad814febc706283c3ff90b89718350
Bui Nam Ngoc <onggia23197@gmail.com>
2024-02-22 23:53:07 +0700

    push code Example 51

--- src/test/java/com/eazybytes/eazyschool/Example51ApplicationTests.java
+++ src/test/java/com/eazybytes/eazyschool/Example51ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EazyschoolApplicationTests {
+class Example51ApplicationTests {
 
 	@Test
 	void contextLoads() {
